de3d89af207a5d266a301bdb39ea74f03b66cef2
Krystal Lopez <lopezkrystal10@gmail.com>
2022-08-30 18:14:44 -0700

    initial commit, created LogInView,SignUpView, and ProductsIndex, added routers and paths

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <router-link to="/">Home</router-link>
     |
     <router-link to="/about">About</router-link>
+    |
+    <router-link to="/signup">Sign Up</router-link>
+    |
+    <router-link to="/login">Log In</router-link>
+    |
+    <router-link to="/products">All Products</router-link>
   </nav>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import SignUpView from "../views/SignUpView.vue";
+import LogInView from "../views/LogInView.vue";
+import ProductsIndex from "../views/ProductsIndex.vue";
 
 const routes = [
   {
@@ -14,6 +17,24 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUpView,
+  },
+
+  {
+    path: "/login",
+    name: "login",
+    component: LogInView,
+  },
+
+  {
+    path: "/products",
+    name: "products-index",
+    component: ProductsIndex,
   },
 ];
 
